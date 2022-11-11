4233292931014de7868029b0477d8553f0d4e0e5
valeriajanzen <janzen_valeria@outlook.com>
2022-11-11 14:00:01 -0300

    topicos

--- forum-30/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum-30/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,15 +1,19 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,6 +23,8 @@ public class TopicosController {
     @Autowired
     private TopicoRepository topicoRepository;
 
+    @Autowired
+    private CursoRepository cursoRepository;
     @RequestMapping("/topicos")
     public List<TopicoDto> lista(String nomeCurso) {
         if (nomeCurso == null) {
@@ -28,5 +34,15 @@ public class TopicosController {
             List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
             return TopicoDto.converter(topicos);
         }
+    }
+
+    @PostMapping
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+        Topico topico = form.converter(cursoRepository);
+        topicoRepository.save(topico);
+
+        URI uri = uriBuilder.path("/topico/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDto(topico));
+
     }
 }
--- forum-30/src/main/java/br/com/alura/forum/modelo/Topico.java
+++ forum-30/src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -24,6 +24,12 @@ public class Topico {
 	public Topico() {
 	}
 
+	public Topico(String titulo, String mensagem, Curso curso) {
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
